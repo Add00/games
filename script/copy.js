aac936e6ab74b9c87b15900181e634863fccc4a3
Luiz Bills <luizbills@pm.me>
2024-11-03 20:41:40 -0300

    rename litecanvas folder to common

--- script/copy.js
+++ script/copy.js
@@ -3,10 +3,10 @@ import { copyFile, mkdir } from "node:fs/promises";
 const root = process.env["PWD"];
 
 const files = {
-    'node_modules/litecanvas/dist/dist.js': 'litecanvas/dist.js'
+    'node_modules/litecanvas/dist/dist.js': 'common/litecanvas.js'
 }
 
-await mkdir(`${root}/litecanvas`)
+await mkdir(`${root}/common`)
 
 for (const [from, to] of Object.entries(files)) {
     await copyFile(`${root}/${from}`, `${root}/${to}`)
