c822939a56d942688929d39f6f80ebe72ed3e9e0
Luiz Bills <luizbills@pm.me>
2024-11-14 21:09:49 -0300

    add comments

--- games/asteroids/graphics.js
+++ games/asteroids/graphics.js
@@ -1,6 +1,11 @@
 function makeGraphics(engine) {
     const res = {};
 
+    /**
+     * The `paint()` function can be used to create a image
+     * using the canvas context or litecanvas' functions to draw graphics.
+     * Then you draw that generated image using the `image(x, y, img)` function.
+     */
     res.ship = engine.paint(60, 60, (ctx) => {
         const w = ctx.canvas.width,
             h = ctx.canvas.height;
@@ -24,35 +29,36 @@ function makeGraphics(engine) {
         engine.fill(11);
     });
 
-    function _drawAsteroid(ctx, sides, radius) {
-        const points = _makeAsteroidShape(sides, radius);
-        ctx.beginPath();
-        for (let i = 0; i < points.length; i++) {
-            if (0 === i) {
-                ctx.moveTo(points[i][0], points[i][1]);
-            } else {
-                ctx.lineTo(points[i][0], points[i][1]);
-            }
-        }
-        ctx.lineTo(points[0][0], points[0][1]);
-    }
-
-    function _makeAsteroidShape(sides, radius) {
-        const points = [];
-        const x = radius;
-        const y = radius;
-        const variation = radius * 0.1; // radius variation
-
-        for (let i = 0; i < sides; i++) {
-            const r = radius + engine.randi(-variation, variation);
-            points.push([
-                x + r * engine.cos((i * engine.TWO_PI) / sides),
-                y + r * engine.sin((i * engine.TWO_PI) / sides),
-            ]);
-        }
-
-        return points;
-    }
-
+    // return an object with all generated images
     return res;
+}
+
+function _drawAsteroid(ctx, sides, radius) {
+    const points = _makeAsteroidShape(sides, radius);
+    ctx.beginPath();
+    for (let i = 0; i < points.length; i++) {
+        if (0 === i) {
+            ctx.moveTo(points[i][0], points[i][1]);
+        } else {
+            ctx.lineTo(points[i][0], points[i][1]);
+        }
+    }
+    ctx.lineTo(points[0][0], points[0][1]);
+}
+
+function _makeAsteroidShape(sides, radius) {
+    const points = [];
+    const x = radius;
+    const y = radius;
+    const variation = radius * 0.1; // radius variation
+
+    for (let i = 0; i < sides; i++) {
+        const r = radius + engine.randi(-variation, variation);
+        points.push([
+            x + r * engine.cos((i * engine.TWO_PI) / sides),
+            y + r * engine.sin((i * engine.TWO_PI) / sides),
+        ]);
+    }
+
+    return points;
 }
